e8e235b5b1d917eb2ace6e4b5f0306b35dac4fd2
Pragyay <p.ksingh562001@gmail.com>
2022-03-21 23:58:47 +0530

    Input values displayed as user changes parameters added descriptive comments made code cleaner

--- rules.js
+++ rules.js
@@ -1,51 +1,47 @@
+// function that draws the fractals
 function iterate(){
     for(let j=0; j<100; j++){
-        let rand = Math.random();
-        let rule;
-        for(let i = 0; i < 4; i++){
-            let r = rules[i];
-            // console.log(r.W);
-            if(rand < r.W){
-                rule = r;
-                break;
-            }else{  
-                rand -= r.W;
-            }
-        }
-        // console.log(`${j}th iteration`);
+        // choose a rule
+        let rule = getRule(rules);
+        
+        // use the rule parameters to get X and Y coordinates
         let x1 = x * rule.A + y * rule.B + rule.E,
             y1 = x * rule.C + y * rule.D + rule.F;
-        // console.log(`in iterate()${i}`);
+        
         x = x1;
         y = y1;
 
         // console.log(`x = ${x}, y = ${y}`);
         plot(x, y);
     }
+
     requestAnimationFrame(iterate);
 }
 
-// function getRule(rules){
-//     let rand = Math.random();
-//     for(let i = 0; i < 4; i++){
-//         let rule = rules[i];
-//         console.log(rule);
-//         if(rand < rule.W){
-//             return rule;
-//         }
-//         // console.log("getRule() done");
-//         rand -= rule.W;
-//     }
-// }
-
-function plot(x, y){
-    context.fillStyle = "black";
-    context.fillRect(x * 60, -y * 60, 1, 1);
+// choosing a rule based on specified weight
+function getRule(rules){
+    let rand = Math.random();
+    for(let i = 0; i < 4; i++){
+        let rule = rules[i];
+        if(rand < rule.W){
+            return rule;
+        }
+        rand -= rule.W;
+    }
 }
 
+// plot a rectangle at position (x,y)
+function plot(x, y){
+    context.fillStyle = "white";
+    context.fillRect(x * 100, -y * 100, 1, 1);
+}
+
+// initializes rules array
 function init(rules){
 
     for(let i=1;i<=4;i++){
+
+        // get all input sliders by ID
         let a = document.getElementById(`a${i}`),
             b = document.getElementById(`b${i}`),
             c = document.getElementById(`c${i}`),
@@ -54,8 +50,7 @@ function init(rules){
             f = document.getElementById(`f${i}`),
             weight = document.getElementById(`weight${i}`);
         
-        // console.log("added parameters");
-
+        // add values of all input sliders to corresponding parameters of the rule
         let rule = {
             A: parseFloat(a.value),
             B: parseFloat(b.value),
@@ -66,12 +61,65 @@ function init(rules){
             W: parseFloat(weight.value)
         };
 
-        // console.log(rule);  
-    
+        // display values in textviews above the sliders
+        let div = document.getElementById(`div${i}`);
+
+        let a_para = div.getElementsByTagName("p")[0],
+            b_para = div.getElementsByTagName('p')[1],
+            c_para = div.getElementsByTagName('p')[2],
+            d_para = div.getElementsByTagName('p')[3],
+            e_para = div.getElementsByTagName('p')[4],
+            f_para = div.getElementsByTagName('p')[5],
+            weight_para = div.getElementsByTagName('p')[6];
+
+        a_para.innerHTML = `A: ${rule.A}`;
+        b_para.innerHTML = `B: ${rule.B}`;
+        c_para.innerHTML = `C: ${rule.C}`;
+        d_para.innerHTML = `D: ${rule.D}`;
+        e_para.innerHTML = `E: ${rule.E}`;
+        f_para.innerHTML = `F: ${rule.F}`;
+        weight_para.innerHTML = `Weight: ${rule.W}`;
+        
+        // append rule to rules[] array
         rules[i-1] = rule;
     }
     console.log(rules);
 }
+
+// update text field when user changes value
+function updateValue(ele, value){
+    // get id of element that called updateValue() function
+    let id = ele.id;
+    
+    let obj = document.getElementById(`${id}`);
+
+    // little string manipulation to display parameter name in textview
+    let parameter = id.slice(0,-1);
+    parameter = parameter.charAt(0).toUpperCase() + parameter.slice(1);
+
+    // update textview as user changes input
+    obj.previousElementSibling.innerHTML = `${parameter}: ${value}`;
+}
+
+// update parameters and canvas when apply button is clicked
+let applyBtn = document.getElementById("apply");
+applyBtn.addEventListener('click', function(){
+    // clear canvas
+    context.clearRect(-width/2, -height, width, height);
+
+    // clear rules array
+    rules = [];
+
+    // choose new X and Y values randomly
+    x = Math.random();
+    y = Math.random();
+
+    // reinitialize rules array
+    init(rules); 
+    
+    // render fractal with new parameter
+    iterate();
+}); 
 
 let canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
@@ -80,29 +128,10 @@ let canvas = document.getElementById("canvas"),
     rules = [];
 
 context.translate(width/2, height);
+
 init(rules);
-// console.log("breaks after init()");
 
 let x = Math.random(),
     y = Math.random();
 
 iterate();
-// console.log("breaks after iterate()");
-
-let applyBtn = document.getElementById("apply");
-applyBtn.addEventListener('click', function(){
-    console.log("apply btn clicked");
-    context.clearRect(-width/2, -height, width, height);
-    rules = [];
-
-    x = Math.random();
-    y = Math.random();
-
-    init(rules); 
-    // console.log("breaks after init()");
-    
-    
-    iterate();
-    // console.log("breaks after iterate()");
-    
-}); 
